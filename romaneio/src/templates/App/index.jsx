7ceeb60a791fd1a01a0719bede58cc6b270a217a
Calebe Rodrigues <calebebrodrigues@gmail.com>
2024-02-28 11:26:10 -0300

    Tabela info

--- romaneio/src/templates/App/index.jsx
+++ romaneio/src/templates/App/index.jsx
@@ -3,6 +3,7 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'bootstrap/dist/js/bootstrap.bundle';
+import { Table } from '../../components/Table';
 
 export const App = () => {
   return (
@@ -10,10 +11,15 @@ export const App = () => {
       <div className="container">
         <div className="mt-4"></div>
         <div className="mt-4"></div>
+
         <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Digite o número da ordem" aria-label="Digite o número da ordem" aria-describedby="button-addon2" />
             <button class="btn btn-outline-secondary" type="button" id="button-addon2">Procurar</button>
         </div>
+
+        <div className="mb-4"></div>
+
+        <Table />
       </div>
     </div>
   );
